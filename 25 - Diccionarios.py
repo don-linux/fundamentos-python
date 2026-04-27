1255568f168ff8700b2f89ee01cdda4b494f50e9
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2026-04-27 11:38:18 -0600

    ✨ Expand dictionary example with comprehensive operations, including addition, update, deletion, and nested dictionaries.

--- 25 - Diccionarios.py
+++ 25 - Diccionarios.py
@@ -1,23 +1,113 @@
+"""
+Coleccion de pares clave-valor
+Ordenados a partir de Python 3.7
+"""
+
 cafe = {
     "grano": "geisha",
     "pais": "colombia",
     "año": 2025,
 }
-
 print(cafe)
-print(cafe["pais"])
-print(cafe.get("grano"))
-print(cafe.keys())
-print(cafe.values())
+
+
+"""
+Recorrer un diccionario
+
+Buscar una clave en el diccionario
+"""
 
 if "grano" in cafe:
     print('Si hay!')
 
-cafe["año"] = 2020
+for k in cafe:
+    print(k)
 
-print(cafe)
+for v in cafe.values():
+    print(v)
 
+"""
+Bucle anidado
+"""
+for k, v in cafe.items():
+    print(k, v)
+
+
+"""
+Acceder a los valores del diccionario, usando corchetes 
+para acceder a la clave y obtener el valor.
+"""
+print(cafe["pais"])
+
+"""
+Usando el metodo get()
+"""
+print(cafe.get("grano"))
+
+"""
+Imprimir las claves del diccionario
+"""
+print(cafe.keys())
+
+"""
+Imprimir los valores del diccionario
+"""
+
+print(cafe.values())
+
+
+"""
+Agregar un nuevo par clave-valor
+"""
 cafe["tostado"] = "Suave"
 print(cafe)
 
-cafe.update({"año": 2022})
+"""
+Actualizar un par clave-valor (claves existentes)
+"""
+cafe["año"] = 2020
+print(cafe)
+
+"""
+Actualizar o agregar un par clave-valor con update()
+"""
+cafe.update({"año": 2022, "grano": "arabica"})
+
+
+"""
+Eliminar un par clave-valor
+"""
+cafe.pop("año")
+print(cafe)
+
+"""
+Eliminar el ultimo par clave-valor
+"""
+cafe.popitem()
+print(cafe)
+
+"""
+Eliminar todos los pares clave-valor
+"""
+cafe.clear()
+print(cafe)
+
+"""
+Eliminar el diccionario completo
+"""
+#del cafe
+#print(cafe)
+
+"""
+Diccionario anidado
+"""
+autos = {
+    "tesla" : {
+        "model" : "X"
+    },
+    "cybertruck" : {
+        "model" : "Founders"
+    }
+}
+
+print("Model", autos["tesla"]["model"])
